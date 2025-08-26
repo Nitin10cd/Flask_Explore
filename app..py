965e9e9550d1026c4ee660eab2e6952582a48b36
Nitin10cd <nsaxenacse@gmail.com>
2025-08-26 09:23:39 +0530

    Advance orm Handling in flask

--- app..py
+++ app..py
@@ -1,24 +1,18 @@
-# ADVANCE FORM HANDLING IN THE PYTHON 
-# => Using the req.form in flask
-
 from flask import Flask, render_template, request
 
-# app creation 
+# App creation
 app = Flask(__name__)
 
-# feedback shareing route dynamic
-@app.route("/feedback", methods = ["POST", "GET"])
+# Feedback route
+@app.route("/feedback", methods=["GET", "POST"])
 def feed_back():
     if request.method == "POST":
         name = request.form.get("username")
         message = request.form.get("message")
-
-        return render_template("thankyou.html", user = name, message = message)
+        return render_template("thankyou.html", user=name, message=message)
+    
+    # GET request will render the feedback form
+    return render_template("feedback.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
